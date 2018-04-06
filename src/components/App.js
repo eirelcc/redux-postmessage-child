b10686f81ed01ee39b6e35d46823fb5f51e7ec12
eirelcc <eirelcc@gmail.com>
2018-04-06 20:35:27 +0300

    Show parentURL only when available

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App({ parentURL, message, onClick }) {
     return (
         <div className="App">
             <h1 className="App-title">IFRAME CONTENT</h1>
-            <div>Parent: {parentURL}</div>
+            {!!parentURL && <div>Parent: {parentURL}</div>}
             <button onClick={onClick} style={{ width: 200, height: 50 }}>
                 SEND MESSAGE
             </button>
